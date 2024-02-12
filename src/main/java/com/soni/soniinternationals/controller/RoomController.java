b30439de34bc4b60f6109a195d169c71d79612e0
prajwalsoni <prajwalsonics@gmail.com>
2024-02-12 21:54:53 +0530

    Added Room Controller

--- src/main/java/com/soni/soniinternationals/controller/RoomController.java
+++ src/main/java/com/soni/soniinternationals/controller/RoomController.java
@@ -123,9 +123,6 @@ public class RoomController {
         }
     }
 
-
-
-
     private RoomResponse getRoomResponse(Room room) {
         List<BookedRoom> bookings = getAllBookingsByRoomId(room.getId());
        List<BookingResponse> bookingInfo = bookings

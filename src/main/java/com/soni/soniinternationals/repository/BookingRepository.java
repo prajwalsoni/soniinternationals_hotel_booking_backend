7f6b69989826167c73fa391a638b50dac5a98503
prajwalsoni <prajwalsonics@gmail.com>
2024-02-12 22:05:26 +0530

    Added Booking and Role Repository

--- src/main/java/com/soni/soniinternationals/repository/BookingRepository.java
+++ src/main/java/com/soni/soniinternationals/repository/BookingRepository.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 
-
 public interface BookingRepository extends JpaRepository<BookedRoom, Long> {
 
     List<BookedRoom> findByRoomId(Long roomId);

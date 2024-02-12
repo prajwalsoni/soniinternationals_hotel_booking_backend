77e07c81e7842f86662a9d814694092cd80cbf8d
prajwalsoni <prajwalsonics@gmail.com>
2024-02-12 22:06:09 +0530

    Added RoomRepository and UserRepository

--- src/main/java/com/soni/soniinternationals/repository/RoomRepository.java
+++ src/main/java/com/soni/soniinternationals/repository/RoomRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
 import java.util.List;
 
-
-
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
     @Query("SELECT DISTINCT r.roomType FROM Room r")

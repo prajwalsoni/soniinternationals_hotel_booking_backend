29da73db576513232a6f145dca62530ee7cbd8ba
prajwalsoni <prajwalsonics@gmail.com>
2024-02-12 22:00:24 +0530

    Added ResourceNotFoundExcetion

--- src/main/java/com/soni/soniinternationals/exception/ResourceNotFoundException.java
+++ src/main/java/com/soni/soniinternationals/exception/ResourceNotFoundException.java
@@ -1,5 +1,6 @@
 package com.soni.soniinternationals.exception;
 
+
 @SuppressWarnings("serial")
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
--- src/main/java/com/soni/soniinternationals/exception/RoleAlreadyExistException.java
+++ src/main/java/com/soni/soniinternationals/exception/RoleAlreadyExistException.java
@@ -1,7 +1,6 @@
 package com.soni.soniinternationals.exception;
 
 
-
 @SuppressWarnings("serial")
 public class RoleAlreadyExistException extends RuntimeException {
     public RoleAlreadyExistException(String message) {

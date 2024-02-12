af0e49c758e742aa03ee633d9708eee2aeb2ac91
prajwalsoni <prajwalsonics@gmail.com>
2024-02-12 22:10:40 +0530

    Added WebSecurityConfig

--- src/main/java/com/soni/soniinternationals/security/WebSecurityConfig.java
+++ src/main/java/com/soni/soniinternationals/security/WebSecurityConfig.java
@@ -19,8 +19,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
-
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
